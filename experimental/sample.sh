2353d06350476c8dd4e2a85036708b48b53c9de7
Christian Tschenett <christian@tschenett.ch>
2025-05-07 16:29:21 +0200

    go transpile sample

--- experimental/sample.sh
+++ experimental/sample.sh
@@ -11,3 +11,9 @@ echo "Example running on IJ-based interpreter running on Go-based interpreter (t
 
 echo Example running on IJ-based interpreter running on IJ-based interpreter running on Java-based interpreter
 ./program_input_feeder.rb sample.ss|./selfhosted_interpreter.sh
+
+echo "Example transpiled to Go, compiled with go build, and executed"
+(cat sample.ss | sed '$d' && echo "//<GO2>") | ./interpreter_mac_arm64 | bash
+rm app.go
+./app
+rm app
