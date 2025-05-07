2ffcf53ac1436d723758433e400e0858739e9e4f
Christian Tschenett <christian@tschenett.ch>
2025-05-07 15:28:47 +0200

    most recent version of IJ interpreter

--- experimental/sample.sh
+++ experimental/sample.sh
@@ -3,10 +3,10 @@
 echo "Example running on Java-based interpreter"
 ./java_interpreter.sh sample.ss
 
-echo "Example running Go-based interpreter (transpiled from IJ-based interpreter)"
+echo "Example running on Go-based interpreter (transpiled from IJ-based interpreter)"
 ./program_input_feeder.rb sample.ss|./interpreter_mac_arm64
 
-echo "Example running on IJ-based interpreter running Go-based interpreter (transpiled from IJ-based interpreter)"
+echo "Example running on IJ-based interpreter running on Go-based interpreter (transpiled from IJ-based interpreter)"
 ./program_input_feeder.rb sample.ss|./hosted_interpreter.sh
 
 echo Example running on IJ-based interpreter running on IJ-based interpreter running on Java-based interpreter
